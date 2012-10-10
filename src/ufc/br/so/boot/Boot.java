02da11b349e655246842659bbfa6afe688e9dedd
Victor Farias <victor.aefarias@gmail.com>
2012-10-10 14:20:39 -0300

    Primeiro boot e shell

--- src/ufc/br/so/boot/Boot.java
+++ src/ufc/br/so/boot/Boot.java
@@ -1,5 +1,52 @@
 package ufc.br.so.boot;
 
-public class Boot {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+import ufc.br.so.commandline.Shell;
+
+public class Boot {
+	
+	private int cpuNum;
+	private int ramSize;
+	private int HDSize;
+
+	public Boot(){
+		getConfig();
+		new Shell();
+	}
+
+	private void getConfig(){
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream("resources/config.cfg"));
+		} catch (IOException e) {
+			System.out.println("No config.cfg found!");
+		}
+		cpuNum = Integer.parseInt(properties.getProperty("CPU_NUM"));
+		ramSize = Integer.parseInt(properties.getProperty("RAM_SIZE"));
+		HDSize = Integer.parseInt(properties.getProperty("HD_SIZE"));
+//		System.out.println(cpuNum+" "+ramSize+" "+HDSize);
+	}
+	
+
+	public int getCpuNum() {
+		return cpuNum;
+	}
+	public void setCpuNum(int cpuNum) {
+		this.cpuNum = cpuNum;
+	}
+	public int getRamSize() {
+		return ramSize;
+	}
+	public void setRamSize(int ramSize) {
+		this.ramSize = ramSize;
+	}
+	public int getHDSize() {
+		return HDSize;
+	}
+	public void setHDSize(int hDSize) {
+		HDSize = hDSize;
+	}
 }
--- src/ufc/br/so/commandline/Shell.java
+++ src/ufc/br/so/commandline/Shell.java
@@ -1,5 +1,32 @@
 package ufc.br.so.commandline;
 
-public class Shell {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Shell {
+	public Shell(){
+		interFace();
+	}
+	private void interFace(){
+		String command = null;
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			System.out.print(">");
+			try {
+				command = buf.readLine();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			executeCommand(command);
+		}
+	}
+	private void executeCommand(String command) {
+		// TODO Auto-generated method stub
+		if(command.compareTo("exit")==0)
+			System.exit(0);
+		else
+			System.out.println("Command not found");
+	}
 }

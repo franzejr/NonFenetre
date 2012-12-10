2d6a56175ecd1db1c3e5a2639d2233ad0504a8e2
Joshua Vicentini <joshvicentini@gmail.com>
2012-12-10 00:14:22 -0300

    jUnit

---
+++ src-test/ufc/br/so/test/SJF.java
@@ -0,0 +1,24 @@
+package ufc.br.so.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SJF {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testExecuteListOfProcessParameters() {
+		fail("Not yet implemented");
+	}
+
+}
